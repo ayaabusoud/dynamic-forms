90f201bb71d716fccbc5c92f46fdd78dc85c9be4
ayaabusoud <90638581+ayaabusoud@users.noreply.github.com>
2023-09-06 09:53:32 +0300

    update question component

--- src/components/addQuestion/AddQuestion.js
+++ src/components/addQuestion/AddQuestion.js
@@ -1,59 +1,66 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import AddOptions from './AddOptions';
 import { useForms } from '../../context/FormsContext';
-import { CHECKBOXES, CHECKBOXES_GRID, DROPDOWN, MULTIPLE_CHOICE, MULTIPLE_CHOICE_GRID, TABLE, TEXT, resetProperties } from '../../utlis/FormUtlis';
+import AnswerTypesDropdown from '../answerTypesDropdown/AnswerTypesDropdown';
+import DeleteButton from '../buttons/deleteButton/DeleteButton';
+import AddTableDimensions from '../tableDimensions/AddTableDimensions';
 
 /**
- * Component for rendering the dropdown menu of answer types.
+ * Component for adding and managing a question.
  *
- * @param {string} selectedAnswerType - The currently selected answer type.
- * @param {number} questionId - The ID of the associated question.
- * @param {function} setAnswerType - A function to set the selected answer type.
- * @returns {JSX.Element} - The rendered answer type dropdown component.
+ * @param {Object} question - The question object containing question details.
+ * @param {number} id - The unique ID of the question.
+ * @returns {JSX.Element} - The rendered question component.
  */
-export default function AnswerTypesDropdown({ selectedAnswerType, questionId, setAnswerType }) {
+export default function AddQuestion({ question, id }) {
+    const [selectedAnswerType, setAnswerType] = useState('Text');
+    const [questionValue, setQuestionValue] = useState('');
     const { formQuestions, setFormQuestions } = useForms();
 
-    let AnswersOptions = [TEXT, CHECKBOXES, DROPDOWN, MULTIPLE_CHOICE, TABLE, MULTIPLE_CHOICE_GRID, CHECKBOXES_GRID]
+    //Updates the question details when selectedAnswerType or questionValue changes.
+    useEffect(() => {
+        updateQuestion();
+    }, [selectedAnswerType, questionValue]);
 
-    /**
-     * Handles the selection of an answer type and performs actions accordingly.
-     *
-     * @param {string} option - The selected answer type.
-     */
-    const handleSelectType = (option) => {
-        setAnswerType(option);
-
-        if (option === TEXT) {
-            resetTextQuestion();
-        }
-    };
-
-    function resetTextQuestion() {
-        let propertiesToDelete = ['options', 'rows', 'columns'];
-
-        const updatedArrayOfQuestions = formQuestions.map((question) => {
-            if (question.id === questionId) {
-                return resetProperties(question, propertiesToDelete);
+    //Updates the question details in formQuestions.
+    function updateQuestion() {
+        const updatedArrayOfQuestions = formQuestions.map((q) => {
+            if (q.id === id) {
+                return { ...q, answerType: selectedAnswerType, question: questionValue };
             }
-            return question;
+            return q;
         });
 
         setFormQuestions(updatedArrayOfQuestions);
     }
 
-    let listItems = AnswersOptions.map((option, index) =>
-        <li key={index} className="dropdown-item" href="#" onClick={() => handleSelectType(option, questionId)}>{option}</li>);
+
+    function deleteQuestion(id) {
+        //delete question from formQuestions where question id === id 
+    }
 
     return (
-        <div>
-            <button
-                className="btn btn-outline-secondary dropdown-toggle"
-                type="button"
-                data-bs-toggle="dropdown"
-                aria-expanded="false">
-                {selectedAnswerType}
-            </button>
-            <ul className="dropdown-menu dropdown-menu-end">{listItems}</ul>
-        </div>
-    )
+        <>
+            <div className="input-group my-3">
+                <input
+                    type="text"
+                    className="form-control"
+                    aria-label="Text input with dropdown button"
+                    placeholder={`Question ${question.id}`}
+                    value={questionValue}
+                    onChange={(e) => setQuestionValue(e.target.value)}
+                />
+
+                <AnswerTypesDropdown selectedAnswerType={selectedAnswerType} questionId={id} setAnswerType={setAnswerType} />
+
+                <DeleteButton deleteFunction={() => deleteQuestion(id)} />
+            </div>
+
+            {selectedAnswerType === 'Text' ? null : selectedAnswerType === 'Multiple Choice Grid' || selectedAnswerType === 'Checkboxes Grid' || selectedAnswerType === 'Table' ? (
+                <AddTableDimensions questionId={id} />
+            ) : (
+                <AddOptions questionId={id} />
+            )}
+        </>
+    );
 }
